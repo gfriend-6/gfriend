705d1ae3b75517b1251087520a0f7263dc76a0e2
gfriend-6 <terrifiedpigeon4@gmail.com>
2024-09-17 19:02:20 -0400

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,3 @@
 <p>please click the timeline sub-page in the nav bar</p>
 
-<p><a href="https://gfriend-6.github.io/gfriend/timeline">or click here</a></p>
+<p><a class="underline text-lg text-gfriend-purple-500" href="https://gfriend-6.github.io/gfriend/timeline">or click here</a></p>
