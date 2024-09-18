5152419462394ebdbd4881e354b2dacf56fd3561
gfriend-6 <terrifiedpigeon3@gmail.com>
2024-09-18 13:28:59 -0400

    update

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,7 +8,7 @@ const config = {
 			fallback: '/404.html'
 		}),
 		paths: {
-			base: "/gfriend"
+			base: process.env.NODE_ENV === "production" ? "" : ""
 		}
 	},
   preprocess: vitePreprocess()
