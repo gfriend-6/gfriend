c81b30c0e37bb00529e3bbd33d82aa293e0ad09a
gfriend-6 <terrifiedpigeon4@gmail.com>
2025-03-11 19:30:48 -0400

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,30 @@
 <h1 class="text-xl font-bold">Coming Soon</h1>
 <ul class="list-disc list-inside">
-<li>Source Music 2018/2019 scrapped girl group debut</li>
-<li>Source Music 2018-2021 female trainees</li>
-<li>GLAM, Source Music and BigHits's first girl group</li>
-<li>Why was Source Music in so much debt in 2019 onward?</li>
-<li>GFriend's trainee days</li>
-<li>Source Music and BigHit's connection prior to acquisition</li>
+    <li>Source Music</li>
+    <ul>
+        <li>Source Music 2018/2019 scrapped girl group debut</li>
+        <li>Source Music 2018-2021 female trainees</li>
+        <li>GLAM, Source Music (and BigHits)'s first girl group</li>
+        <li>Why has Source Music been in so much debt in since before they joined HYBE?</li>
+        <li>GFriend's trainee days</li>
+        <li>Source Music and BigHit's connection prior to acquisition</li>
+    </ul>
+    <li>PLEDIS</li>
+    <ul>
+        <li>PLEDIS's neglect of NU'EST</li>
+        <li>PLEDIS's neglect of Pristin</li>
+        <li>PLEDIS's neglect of fromis_9</li>
+    </ul>
+    <li>ADOR</li>
+    <ul>
+        <li>From "N Team" to "NewJeans"</li>
+        <li>NJZ vs HYBE</li>
+    </ul>
 </ul>
 <br /><br />
-<p>For any corrections or requests: <a class="text-primary-3 underline hover:text-primary-4" href="https://x.com/EyeOfTheStormDB">@EyeOfTheStormDB</a></p>
+<p>
+    For any corrections or requests: <a
+        class="text-primary-3 underline hover:text-primary-4"
+        href="https://x.com/EyeOfTheStormDB">@EyeOfTheStormDB</a
+    >
+</p>
