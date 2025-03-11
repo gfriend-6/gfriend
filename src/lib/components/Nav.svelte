3b7fc6c620634bf3198625537931946c8cfe0f3b
gfriend-6 <terrifiedpigeon4@gmail.com>
2025-03-11 10:47:19 -0400

    Update Nav.svelte

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -2,18 +2,18 @@
   import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, Dropdown, DropdownItem, DropdownDivider } from 'flowbite-svelte';
   import { ChevronDownOutline } from 'flowbite-svelte-icons';
   import { page } from '$app/stores';
-  import logo from '$lib/images/logo.svg';
+  import logo from '$lib/images/logo.png';
 
   $: activeUrl = $page.url.pathname;
 </script>
 <div class="w-full bg-accent-3">
-<Navbar color="primary" class="w-4/5 place-self-center mx-auto">
+<Navbar class="w-4/5 place-self-center mx-auto bg-color-primary-2">
   <NavBrand href="/">
     <img src={logo} class="me-3 h-6 sm:h-9" alt="logo"/>
     <span class="self-center whitespace-nowrap text-xl font-semibold text-black">Eye of the Storm</span>
   </NavBrand>
   <NavHamburger />
-  <NavUl {activeUrl} activeClass="font-bold text-accent-1" nonActiveClass="text-accent-5 hover:text-white">
+  <NavUl {activeUrl} activeClass="font-bold text-primary-3" nonActiveClass="text-accent-5 hover:text-white">
     <NavLi href="/">Home</NavLi>
     <NavLi href="/timeline">Timeline</NavLi>
   </NavUl>
