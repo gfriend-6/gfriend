8ab6bfe77e57320d6a1b3986dd91fc002d3216a1
gfriend-6 <terrifiedpigeon4@gmail.com>
2025-03-11 16:35:18 -0400

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,10 @@
 <ul class="list-disc list-inside">
 <li>Source Music 2018/2019 scrapped girl group debut</li>
 <li>Source Music 2018-2021 female trainees</li>
+<li>GLAM, Source Music and BigHits's first girl group</li>
+<li>Why was Source Music in so much debt in 2019 onward?</li>
+<li>GFriend's trainee days</li>
+<li>Source Music and BigHit's connection prior to acquisition</li>
 </ul>
 <br /><br />
 <p>For any corrections or requests: <a class="text-primary-3 underline hover:text-primary-4" href="https://x.com/EyeOfTheStormDB">@EyeOfTheStormDB</a></p>
