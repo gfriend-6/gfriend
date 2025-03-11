efda2fd57e626136518ba1ce1df4865c9e76a138
gfriend-6 <terrifiedpigeon4@gmail.com>
2025-03-11 09:50:15 -0400

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,5 @@
 import adapter from '@sveltejs/adapter-cloudflare-workers';
-export default {
-	kit: {
-		adapter: adapter({
-			// see below for options that can be set here
-		})
-	}
-};
+
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
