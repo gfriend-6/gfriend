7d0c34254b9744a4a24307bd0be83305f6c79a34
gfriend-6 <terrifiedpigeon4@gmail.com>
2025-03-11 19:57:39 -0400

    .

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -7,7 +7,7 @@
   $: activeUrl = $page.url.pathname;
 </script>
 <div class="w-full bg-primary-2">
-<Navbar class="w-3/5 place-self-center mx-auto bg-primary-2">
+<Navbar class="w-4/5 place-self-center mx-auto bg-primary-2">
   <NavBrand href="/">
     <img src={logo} class="me-3 size-10" alt="logo"/>
     <span class="self-center whitespace-nowrap text-xl font-semibold text-black">Eye of the Storm</span>
